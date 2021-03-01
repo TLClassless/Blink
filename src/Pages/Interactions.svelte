2efa34502b591aec51f9ab3a7cb5a9a70a37b660
Ben <classle55sc2@gmail.com>
2021-03-01 03:21:05 +0000

    styled and rejigged layout for notifications page

--- src/Pages/Interactions.svelte
+++ src/Pages/Interactions.svelte
@@ -2,41 +2,110 @@
     let username = "TLClassless";
     let commented = "commented:";
     let liked = "liked your post!";
+    let comment = "This is a sample comment!";
+
+    
 </script>
 
 <main>
     <div class="notification">
-        <img class="profile-picture" />
-        <h3 class="interaction-type">{username}</h3> 
-        <img class="content-thumbnail" />
+        <img src="img/games/phasmophobia.png" class="content-thumbnail" alt="thumbnail" />
+        <span class="interaction">
+            <span class="interaction-indentity"><h3 class="interaction-type" id="color">/{username}</h3><span class="divider"></span><h3 class="interaction-type"> {commented}</h3></span>
+            <p class="interaction-type">{comment}</p>
+        </span>
     </div>
     <div class="notification">
-        <img class="profile-picture" />
-        <h3 class="interaction-type">{username}</h3> 
-        <img class="content-thumbnail" />
+        <img src="img/games/phasmophobia.png" class="content-thumbnail" alt="thumbnail" />
+        <span class="interaction">
+            <span class="interaction-indentity"><h3 class="interaction-type" id="color">/{username}</h3><span class="divider"></span><h3 class="interaction-type"> {commented}</h3></span>
+            <p class="interaction-type">{comment}</p>
+        </span>
     </div>
     <div class="notification">
-        <img class="profile-picture" />
-        <h3 class="interaction-type">{username}</h3> 
-        <img class="content-thumbnail" />
+        <img src="img/games/phasmophobia.png" class="content-thumbnail" alt="thumbnail" />
+        <span class="interaction">
+            <span class="interaction-indentity"><h3 class="interaction-type" id="color">/{username}</h3><span class="divider"></span><h3 class="interaction-type"> {commented}</h3></span>
+            <p class="interaction-type">{comment}</p>
+        </span>
     </div>
     <div class="notification">
-        <img class="profile-picture" />
-        <h3 class="interaction-type">{username}</h3> 
-        <img class="content-thumbnail" />
+        <img src="img/games/phasmophobia.png" class="content-thumbnail" alt="thumbnail" />
+        <span class="interaction">
+            <span class="interaction-indentity"><h3 class="interaction-type" id="color">/{username}</h3><span class="divider"></span><h3 class="interaction-type"> {commented}</h3></span>
+            <p class="interaction-type">{comment}</p>
+        </span>
     </div>
     <div class="notification">
-        <img class="profile-picture" />
-        <h3 class="interaction-type">{username}</h3> 
-        <img class="content-thumbnail" />
+        <img src="img/games/phasmophobia.png" class="content-thumbnail" alt="thumbnail" />
+        <span class="interaction">
+            <span class="interaction-indentity"><h3 class="interaction-type" id="color">/{username}</h3><span class="divider"></span><h3 class="interaction-type"> {commented}</h3></span>
+            <p class="interaction-type">{comment}</p>
+        </span>
     </div>
     <div class="notification">
-        <img class="profile-picture" />
-        <h3 class="interaction-type">{username}</h3> 
-        <img class="content-thumbnail" />
+        <img src="img/games/phasmophobia.png" class="content-thumbnail" alt="thumbnail" />
+        <span class="interaction">
+            <span class="interaction-indentity"><h3 class="interaction-type" id="color">/{username}</h3><span class="divider"></span><h3 class="interaction-type"> {commented}</h3></span>
+            <p class="interaction-type">{comment}</p>
+        </span>
+    </div>
+    <div class="notification">
+        <img src="img/games/phasmophobia.png" class="content-thumbnail" alt="thumbnail" />
+        <span class="interaction">
+            <span class="interaction-indentity"><h3 class="interaction-type" id="color">/{username}</h3><span class="divider"></span><h3 class="interaction-type"> {commented}</h3></span>
+            <p class="interaction-type">{comment}</p>
+        </span>
     </div>
 </main>
 
 <style>
+    .divider {
+        width: 0.4rem;
+    }
 
+    .notification {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        margin: 1rem 0;
+    }
+
+    .content-thumbnail {
+        display: flex;
+        height: 2.5rem;
+        overflow: hidden;
+        margin-right: 1rem;
+    }
+
+    .interaction {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+    }
+
+    .interaction-type {
+        margin: 0;
+        font-size: 1rem;
+    }
+
+    .interaction-indentity {
+        display: flex;
+        flex-direction: row;
+    }
+
+    #color {
+        margin: 0;
+        text-transform: uppercase;
+        font-weight: 700;
+        background-color: var(--neon-trees-1);
+        background-image: linear-gradient(45deg, var(--neon-trees-1), var(--neon-trees-2));
+        background-size: 100%;
+        background-clip: text;
+        -webkit-background-clip: text;
+        -moz-background-clip: text;
+        -webkit-text-fill-color: transparent; 
+        -moz-text-fill-color: transparent;
+    }
 </style>
